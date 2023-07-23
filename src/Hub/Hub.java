9389136bc7e740edfe31164de71f7a7e98f6a8fd
Pawuloon <pdembicki1@gmail.com>
2023-07-23 13:47:18 +0200

    up

--- src/Hub/Hub.java
+++ src/Hub/Hub.java
@@ -44,6 +44,8 @@ public class Hub extends JFrame
         // Buttons
         var buttonPanel = new JPanel();
         buttonPanel.setPreferredSize(new Dimension(100, 100));
+        buttonPanel.setLayout(new GridLayout(2, 2, 10, 10));
+        buttonPanel.setBackground(Color.BLACK);
 
         // Pong game button
         var button = new JButton("Ping Pong");
@@ -56,17 +58,37 @@ public class Hub extends JFrame
         });
 
 
-
         // Tic Tac Toe game button
         var button2 = new JButton("Tic Tac Toe");
         button2.setPreferredSize(new Dimension(100, 90));
         button2.addActionListener(e ->
         {
             // TODO Add Tic Tac Toe
+            dispose();
+        });
+
+        // Snake game button
+        var button3 = new JButton("Snake");
+        button3.setPreferredSize(new Dimension(100, 90));
+        button3.addActionListener(e ->
+        {
+            // TODO Add Snake
+            dispose();
+        });
+
+        // Flappy Bird game button
+        var button4 = new JButton("Flappy Bird");
+        button4.setPreferredSize(new Dimension(100, 90));
+        button4.addActionListener(e ->
+        {
+            // TODO Add Flappy Bird
+            dispose();
         });
 
         buttonPanel.add(button);
         buttonPanel.add(button2);
+        buttonPanel.add(button3);
+        buttonPanel.add(button4);
 
         add(backgroundPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
